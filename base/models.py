35bc67a058302dbe8a9566db8608e6a484dab4ed
Dmytro <94nj111@gmail.com>
2024-12-20 13:28:38 +0200

    add apps & base user

---
+++ base/models.py
@@ -0,0 +1,14 @@
+import uuid
+
+from django.db import models
+
+
+class UUIDModel(models.Model):
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, unique=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    changed_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
